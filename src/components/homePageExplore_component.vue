807fb7160c5df2905cc60b2808af34cfd30c3d25
chin <149858139+dangkhoa2004@users.noreply.github.com>
2024-10-14 23:06:38 +0700

    dangkh0a
    upload first time

--- src/components/homePageExplore_component.vue
+++ src/components/homePageExplore_component.vue
@@ -1,33 +1,27 @@
 <template>
-    <div id="explore"></div>
-    <section class="section explore_section hidden" id="">
-        <p class="title">KHÁM PHÁ</p>
-        <h2 class="tagline">Nơi tốt nhất cho bạn có trải nghiệm điện ảnh !</h2>
-
-        <div class="explore_wrapper">
-            <div class="explore_card">
-                <div class="card_img">
-                    <img src="https://i.ibb.co/34Q0xs1/joker.jpg" alt="" />
-                    <div class="card_title">JOKER</div>
-                </div>
-            </div>
-            <div class="explore_card">
-                <div class="card_img">
-                    <img src="https://i.ibb.co/ZhFw2qt/domino.jpg" alt="" />
-                    <div class="card_title">DOMINO</div>
-                </div>
-            </div>
-            <div class="explore_card">
-                <div class="card_img">
-                    <img src="https://i.ibb.co/r5x8cbg/kumanthong.jpg" alt="" />
-                    <div class="card_title">KUMANTHONG</div>
-                </div>
-            </div>
-        </div>
-    </section>
+<div id="explore"></div>
+<section class="section explore_section hidden" id="">
+<p class="title">KHÁM PHÁ</p>
+<h2 class="tagline">Nơi tốt nhất cho bạn có trải nghiệm điện ảnh !</h2>
+<div class="explore_wrapper">
+<div class="explore_card">
+<div class="card_img">
+<img src="../assets/img/joker.webp" alt="" />
+<div class="card_title">JOKER</div>
+</div>
+</div>
+<div class="explore_card">
+<div class="card_img">
+<img src="../assets/img/domino.webp" alt="" />
+<div class="card_title">DOMINO</div>
+</div>
+</div>
+<div class="explore_card">
+<div class="card_img">
+<img src="../assets/img/kumanthong.webp" alt="" />
+<div class="card_title">KUMANTHONG</div>
+</div>
+</div>
+</div>
+</section>
 </template>
-<script>
-export default {
-
-}
-</script>
--- src/components/homePageHeader_component.vue
+++ src/components/homePageHeader_component.vue
@@ -1,58 +1,31 @@
 <template>
-  <header class="header" id="home">
-    <nav>
-      <div class="navbar">
-        <div class="logo">
-          <a href="#">
-            <h2>CINEBOO</h2>
-          </a>
-        </div>
-
-        <div class="menu_btn">
-          <i class="fas fa-bars"></i>
-        </div>
-      </div>
-
-      <ul class="links">
-        <li><a href="#home">TRANG CHỦ</a></li>
-        <li><a href="#movies">PHIM</a></li>
-        <li><a href="#theater">RẠP</a></li>
-        <li><a href="#explore">KHÁM PHÁ</a></li>
-        <li><a href="#contact">HỖ TRỢ</a></li>
-      </ul>
-
-      <button class="btn nav_btn draw-border">Đặt vé ngay</button>
-    </nav>
-    <!-- <nav>
-    <div class="navbar">
-      <ul class="links">
-        <li>
-          <h1>CINEBOO</h1>
-        </li>
-      </ul>
-      <div class="menu_btn">
-        <i class="fas fa-bars"></i>
-      </div>
-    </div>
-
-    <ul class="links">
-      <li><a href="#home">TRANG CHỦ</a></li>
-      <li><a href="#movies">PHIM</a></li>
-      <li><a href="#theater">RẠP</a></li>
-      <li><a href="#explore">KHÁM PHÁ</a></li>
-      <li><a href="#contact">HỖ TRỢ</a></li>
-    </ul>
-
-    <button class="btn draw-border">Đặt vé ngay</button>
-  </nav> -->
-
-    <section class="section header_section">
-      <p>Nhanh chóng - Tiện lợi - Ưu đãi</p>
-      <h1>
-        Điện ảnh đích thực tại <br />
-        <span>CineBoo</span>
-      </h1>
-    </section>
-  </header>
-
+<header class="header" id="home">
+<nav>
+<div class="navbar">
+<div class="logo">
+<a href="#">
+<h2>CINEBOO</h2>
+</a>
+</div>
+<div class="menu_btn">
+<i class="fas fa-bars"></i>
+</div>
+</div>
+<ul class="links">
+<li><a href="#home">TRANG CHỦ</a></li>
+<li><a href="#movies">PHIM</a></li>
+<li><a href="#theater">RẠP</a></li>
+<li><a href="#explore">KHÁM PHÁ</a></li>
+<li><a href="#contact">HỖ TRỢ</a></li>
+</ul>
+<button class="btn nav_btn draw-border">Đặt vé ngay</button>
+</nav>
+<section class="section header_section">
+<p>Nhanh chóng - Tiện lợi - Ưu đãi</p>
+<h1>
+Điện ảnh đích thực tại <br />
+<span>CineBoo</span>
+</h1>
+</section>
+</header>
 </template>
